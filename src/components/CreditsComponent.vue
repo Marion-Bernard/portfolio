5bba428d79df40a40956735fd2865cc689f8d8ea
Marion BERNARD <marion.lautier.bernard@gmail.com>
2025-01-22 18:31:54 +0100

    feat:navbar and footer

---
+++ src/components/CreditsComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="col-6 mx-auto">
+    <h1 class="text-white text-center my-4">&copy; Copyright</h1>
+    <p class="text-white">Les icônes de ce portfolio sont issues du site Flaticon&nbsp;:</p>
+    <ul>
+      <li>
+        <a
+          class="link-white"
+          href="https://www.flaticon.com/fr/icones-gratuites/france"
+          title="france icônes"
+          >France icônes créées par Freepik - Flaticon</a
+        >
+      </li>
+      <li>
+        <a
+          class="link-white"
+          href="https://www.flaticon.com/fr/icones-gratuites/royaume-uni"
+          title="royaume-uni icônes"
+          >Royaume-uni icônes créées par Freepik - Flaticon</a
+        >
+      </li>
+      <li>
+        <a
+          class="link-white"
+          href="https://www.flaticon.com/fr/icones-gratuites/travail"
+          title="travail icônes"
+          >Travail icônes créées par Sicon - Flaticon</a
+        >
+      </li>
+      <li>
+        <a
+          class="link-white"
+          href="https://www.flaticon.com/fr/icones-gratuites/education"
+          title="éducation icônes"
+          >Éducation icônes créées par Freepik - Flaticon</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
--- src/components/Partials/FooterComponent.vue
+++ src/components/Partials/FooterComponent.vue
@@ -1,4 +1,37 @@
-<template
->
-<p>footer</p>
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <div class="footer-container d-flex g-4 mt-4">
+    <a href="https://www.linkedin.com/in/marion-bernard/" target="_blank">
+      <img class="img-logo" src="/icons/icon-linkedin.png" />
+    </a>
+    <a href="https://github.com/Marion-Bernard" target="_blank">
+      <img class="img-logo" src="/icons/icon-github.png" />
+    </a>
+    <RouterLink class="link-white" to="/Credits">&copy; Copyright</RouterLink>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+}
+</script>
+
+<style>
+.footer-container {
+  min-height: 50px;
+  gap: 40px;
+  justify-content: center;
+}
+
+.img-logo {
+  height: 30px;
+  transition: height 0.3s ease;
+}
+.img-logo:hover {
+  height: 32px;
+}
+</style>
